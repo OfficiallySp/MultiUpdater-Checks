80037777328b88e6d545957881ca220acb583dfd
Shane Pepperell <shane.pepperell@me.com>
2023-03-31 00:30:12 +0100

    v1.2 rel

--- Assets/Scripts/Startup.cs
+++ Assets/Scripts/Startup.cs
@@ -8,14 +8,6 @@ using UnityEngine.UI;
 
 public class Startup : MonoBehaviour
 {
-    public string versionurl = "";
-    public string currentver;
-    public string latestver;
-    public GameObject Update;
-
-    public string Date;
-    public string DateLong;
-    public int CurrTime;
 
     // Start is called before the first frame update
     private void Start()
@@ -23,45 +15,9 @@ public class Startup : MonoBehaviour
         Application.backgroundLoadingPriority = ThreadPriority.Low;
         Resources.UnloadUnusedAssets();
         Application.targetFrameRate = 40;
-        StartCoroutine(LoadtxtData(versionurl));
-        Date = DateTime.Now.ToShortDateString();
-        DateLong = DateTime.Now.ToLongDateString();
-        CurrTime = DateTime.Now.Hour;
-    }
-    IEnumerator LoadtxtData(string url)
-    {
-        UnityWebRequest loaded = new UnityWebRequest(url);
-        loaded.downloadHandler = new DownloadHandlerBuffer();
-        yield return loaded.SendWebRequest();
-
-        latestver = loaded.downloadHandler.text;
-    }
-    private void CheckVersion()
-    {
-        Debug.Log("Version = " + currentver);
-        Debug.Log("Latest = " + latestver);
-
-        Version versionLocal = new Version(currentver);
-        Version versionremote = new Version(latestver);
-        int result = versionLocal.CompareTo(versionremote);
-
-        if ((latestver != "") && result < 0)
-        {
-            Update.SetActive(true);
-            Time.timeScale = 0;
-        }
-        else
-        {
-            Time.timeScale = 1;
-        }
-    }
-    public void UpdateApp()
-    {
-        Application.Quit();
     }
     public void CancelUpdate()
     {
-        Update.SetActive(false);
         SceneManager.LoadScene("League");
     }
 }
